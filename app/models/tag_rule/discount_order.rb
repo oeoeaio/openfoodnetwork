73b14affdfe13350cf4b9fa0ce01260ef859b470
Rob Harrington <oeoeaio@gmail.com>
2016-04-08 13:00:45 +1000

    TEMP: Tweaking discount order rule to discount taxes and fees correctly

--- app/models/tag_rule/discount_order.rb
+++ app/models/tag_rule/discount_order.rb
@@ -6,6 +6,7 @@ class TagRule::DiscountOrder < TagRule
   # Warning: this should only EVER be called via TagRule#apply
   def apply!
     create_adjustment(I18n.t("discount"), subject, subject)
+    discount_taxes!
   end
 
   def subject_class_matches?
@@ -19,5 +20,30 @@ class TagRule::DiscountOrder < TagRule
 
   def already_applied?
     subject.adjustments.where(originator_id: id, originator_type: "TagRule").any?
+  end
+
+  def compute_amount(calculable)
+    super(calculable) + compute_discount_on_fees(calculable)
+  end
+
+  def compute_discount_on_fees(calculable)
+    fee_total = (discountable_adjustments(calculable).enterprise_fee).sum &:amount
+    value = fee_total * BigDecimal(calculator.preferred_flat_percent.to_s) / 100.0
+    (value * 100).round.to_f / 100
+  end
+
+  def discount_taxes!
+    tax_adjustments = discountable_adjustments.where(originator_type: "Spree::TaxRate")
+    discountable_adjustments.update_all("included_tax = included_tax + ROUND(included_tax * #{calculator.preferred_flat_percent/100},2)")
+    tax_adjustments.update_all("amount = amount + ROUND(amount * #{calculator.preferred_flat_percent/100},2)")
+  end
+
+  def discountable_adjustments(calculable=subject)
+    ids = (calculable.adjustments + calculable.price_adjustments - non_discountable_adjustments(calculable)).map(&:id)
+    Spree::Adjustment.where(id: ids)
+  end
+
+  def non_discountable_adjustments(calculable)
+    calculable.adjustments.where("(originator_id = (?) AND originator_type = (?)) OR originator_type = (?)", id, 'TagRule', 'Spree::ShippingMethod')
   end
 end
--- spec/models/tag_rule/discount_order_spec.rb
+++ spec/models/tag_rule/discount_order_spec.rb
@@ -50,16 +50,16 @@ describe TagRule::DiscountOrder, type: :model do
   describe "applying the rule" do
     # Assume that all validation is done by the TagRule base class
 
-    let!(:line_item) { create(:line_item, price: 100.00) }
-    let!(:order) { line_item.order }
-
-    before do
-      order.update_distribution_charge!
-      tag_rule.calculator.update_attribute(:preferred_flat_percent, -10.00)
-      tag_rule.set_context(order, nil)
-    end
-
     context "in a simple scenario" do
+      let!(:line_item) { create(:line_item, price: 100.00) }
+      let!(:order) { line_item.order }
+
+      before do
+        order.update_distribution_charge!
+        tag_rule.calculator.update_attribute(:preferred_flat_percent, -10.00)
+        tag_rule.set_context(order, nil)
+      end
+
       let(:adjustment) { order.reload.adjustments.where(originator_id: tag_rule, originator_type: "TagRule").first }
 
       it "creates a new adjustment on the order" do
@@ -73,8 +73,14 @@ describe TagRule::DiscountOrder, type: :model do
     end
 
     context "when shipping charges apply" do
+      let!(:line_item) { create(:line_item, price: 100.00) }
+      let!(:order) { line_item.order }
       let!(:shipping_method) { create(:shipping_method, calculator: Spree::Calculator::FlatRate.new( preferred_amount: 25.00 ) ) }
+
       before do
+        order.update_distribution_charge!
+        tag_rule.calculator.update_attribute(:preferred_flat_percent, -10.00)
+        tag_rule.set_context(order, nil)
         shipping_method.create_adjustment("Shipping", order, order, true)
       end
 
@@ -87,6 +93,97 @@ describe TagRule::DiscountOrder, type: :model do
         expect(adjustment.label).to eq "Discount"
         expect(order.adjustment_total).to eq 15.00
         expect(order.total).to eq 115.00
+      end
+    end
+
+    context "complex examples" do
+      let!(:zone)            { create(:zone_with_member) }
+      let(:tax_rate)         { create(:tax_rate, included_in_price: true, calculator: Spree::Calculator::DefaultTax.new, zone: zone, amount: 0.1) }
+      let(:tax_category)     { create(:tax_category, tax_rates: [tax_rate]) }
+
+      let(:coordinator)      { create(:distributor_enterprise, charges_sales_tax: true) }
+      let(:variant)          { create(:variant, product: create(:product, tax_category: nil)) }
+      let(:order_cycle)      { create(:simple_order_cycle, suppliers: [coordinator], coordinator: coordinator, distributors: [coordinator], variants: [variant]) }
+      let!(:order)           { create(:order, order_cycle: order_cycle, distributor: coordinator) }
+      let!(:line_item)       { create(:line_item, order: order, variant: variant, price: 100.00) }
+
+      before do
+        tag_rule.calculator.update_attribute(:preferred_flat_percent, -10.00)
+        tag_rule.set_context(order, nil)
+      end
+
+      context "when enterprise fees with tax apply to the order" do
+        let(:enterprise_fee)   { create(:enterprise_fee, enterprise: coordinator, tax_category: tax_category, calculator: Spree::Calculator::FlatPercentItemTotal.new(preferred_flat_percent: 10)) }
+        let!(:exchange_fee)     { ExchangeFee.create!(exchange: order_cycle.exchanges.incoming.first, enterprise_fee: enterprise_fee) }
+
+        before do
+          enterprise_fee.update_attribute(:tax_category, tax_category)
+          order.update_distribution_charge!
+        end
+
+        let(:adjustment) { order.reload.adjustments.where(originator_id: tag_rule, originator_type: "TagRule").first }
+
+        it "adjustment is made on line item total, and enterprise fee amount and included_tax are adjusted" do
+          tag_rule.send(:apply!)
+          expect(adjustment).to be_a Spree::Adjustment
+          expect(adjustment.amount).to eq -11.00
+          expect(adjustment.label).to eq "Discount"
+          fee_adjustment = order.adjustments.enterprise_fee.first
+          expect(fee_adjustment).to be_a Spree::Adjustment
+          expect(fee_adjustment.amount).to eq 10.00
+          expect(order.adjustment_total).to eq -1.00
+          expect(order.total_tax).to eq 0.82
+          expect(order.total).to eq 99.00
+        end
+      end
+
+      context "when tax applies to line items (included in price)" do
+        before do
+          variant.product.update_attribute(:tax_category, tax_category)
+          order.reload.create_tax_charge!
+          order.update_distribution_charge!
+        end
+
+        let(:adjustment) { order.reload.adjustments.where(originator_id: tag_rule, originator_type: "TagRule").first }
+
+        it "adjustment is made on line item total, and enterprise fee amount and included_tax are adjusted" do
+          tag_rule.send(:apply!)
+          expect(adjustment).to be_a Spree::Adjustment
+          expect(adjustment.amount).to eq -10.00
+          expect(adjustment.label).to eq "Discount"
+          tax_adjustment = line_item.adjustments.where(originator_type: "Spree::TaxRate").first
+          expect(tax_adjustment).to be_a Spree::Adjustment
+          expect(tax_adjustment.amount).to eq 8.18
+          expect(tax_adjustment.included_tax).to eq 8.18
+          expect(order.adjustment_total).to eq -10.00
+          expect(order.total_tax).to eq 8.18 # Total is now $90 because it was discounted by 10%
+          expect(order.total).to eq 90.00
+        end
+      end
+
+      context "when tax applies to the order (not included in price)" do
+        before do
+          tax_rate.update_attribute(:included_in_price, false)
+          variant.product.update_attribute(:tax_category, tax_category)
+          order.reload.create_tax_charge!
+          order.update_distribution_charge!
+        end
+
+        let(:adjustment) { order.reload.adjustments.where(originator_id: tag_rule, originator_type: "TagRule").first }
+
+        it "adjustment is made on order subtotal, amount and included tax on order are adjusted" do
+          tag_rule.send(:apply!)
+          expect(adjustment).to be_a Spree::Adjustment
+          expect(adjustment.amount).to eq -10.00
+          expect(adjustment.label).to eq "Discount"
+          tax_adjustment = order.adjustments.tax.first
+          expect(tax_adjustment).to be_a Spree::Adjustment
+          expect(tax_adjustment.amount).to eq 9.00
+          expect(tax_adjustment.included_tax).to eq 9.00
+          expect(order.adjustment_total).to eq 0.00
+          expect(order.total_tax).to eq 9.00 # Total is now $90 because it was discounted by 10%
+          expect(order.total).to eq 100.00
+        end
       end
     end
   end
