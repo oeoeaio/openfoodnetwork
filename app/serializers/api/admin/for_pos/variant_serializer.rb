3ca37f74dd584bef06bfb55f2096d1bb36ca3cd8
Rob Harrington <oeoeaio@gmail.com>
2017-01-05 11:07:45 +1100

    POS Variant serializer does not need to render price

--- app/serializers/api/admin/for_pos/variant_serializer.rb
+++ app/serializers/api/admin/for_pos/variant_serializer.rb
@@ -1,6 +1,5 @@
 class Api::Admin::ForPos::VariantSerializer < ActiveModel::Serializer
   attributes :id, :full_name, :name_to_display
-  attributes :price
 
   has_one :product, serializer: Api::Admin::IdSerializer
 end
