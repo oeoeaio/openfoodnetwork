58297c85c27a95980d35dc761246c9e3267d6ee4
Rob Harrington <oeoeaio@gmail.com>
2017-10-25 16:11:14 +1100

    Only load content with button click

--- spec/features/lala_spec.rb
+++ spec/features/lala_spec.rb
@@ -10,6 +10,8 @@ feature "Lala", js: true do
     expect(page).to have_content "SHOPS"
     expect(page).to have_content "MAP"
     expect(page).to have_content "ABOUT"
+    sleep 5
+    click_button "lalala"
     # Shows the interface for adding a card
     expect(page).to have_selector '#card-element.StripeElement'
   end
