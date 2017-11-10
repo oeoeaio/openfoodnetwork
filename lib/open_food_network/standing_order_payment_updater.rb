4d91338a66f6be260e356a86fce6513392b22bcc
Rob Harrington <oeoeaio@gmail.com>
2017-11-10 11:36:19 +1100

    Ensure a credit card is present before processing payment

--- lib/open_food_network/standing_order_payment_updater.rb
+++ lib/open_food_network/standing_order_payment_updater.rb
@@ -1,13 +1,15 @@
 module OpenFoodNetwork
   class StandingOrderPaymentUpdater
-
     def initialize(order)
       @order = order
     end
 
     def update!
       return unless payment
+      ensure_credit_card if card_required?
       payment.update_attributes(amount: @order.outstanding_balance)
+    rescue MissingCardError
+      send_payment_failure_email
     end
 
     private
@@ -15,5 +17,25 @@ module OpenFoodNetwork
     def payment
       @payment ||= @order.pending_payments.last
     end
+
+    def card_required?
+      payment.payment_method.type == Spree::Gateway::StripeConnect
+    end
+
+    def ensure_credit_card
+      return if payment.source is_a? Spree::CreditCard
+      raise MissingCardError unless credit_card.present?
+      payment.update_attributes(source: credit_card)
+    end
+
+    def credit_card
+      @credit_card ||= @order.standing_order.credit_card
+    end
+
+    def send_payment_failure_email
+      # Spree::OrderMailer.standing_order_email(order.id, 'confirmation', {}).deliver
+    end
   end
+
+  class MissingCardError < StandardError; end
 end
--- spec/lib/open_food_network/standing_order_payment_updater_spec.rb
+++ spec/lib/open_food_network/standing_order_payment_updater_spec.rb
@@ -54,18 +54,97 @@ module OpenFoodNetwork
       context "when a payment is present" do
         before { allow(updater).to receive(:payment) { payment } }
 
-        context "when the payment total doesn't match the outstanding balance on the order" do
-          before { allow(order).to receive(:outstanding_balance) { 5 } }
-          it "updates the payment total to reflect the outstanding balance" do
-            expect{updater.update!}.to change(payment, :amount).from(10).to(5)
+        context "when a credit card is not required" do
+          before do
+            allow(updater).to receive(:card_required?) { false }
+            expect(updater).to_not receive(:ensure_credit_card)
+          end
+
+          context "when the payment total doesn't match the outstanding balance on the order" do
+            before { allow(order).to receive(:outstanding_balance) { 5 } }
+            it "updates the payment total to reflect the outstanding balance" do
+              expect{updater.update!}.to change(payment, :amount).from(10).to(5)
+            end
+          end
+
+          context "when the payment total matches the outstanding balance on the order" do
+            before { allow(order).to receive(:outstanding_balance) { 10 } }
+
+            it "does nothing" do
+              expect{updater.update!}.to_not change(payment, :amount).from(10)
+            end
           end
         end
 
-        context "when the payment total matches the outstanding balance on the order" do
-          before { allow(order).to receive(:outstanding_balance) { 10 } }
+        context "when a credit card is required" do
+          before do
+            allow(updater).to receive(:card_required?) { true }
+          end
 
-          it "does nothing" do
-            expect{updater.update!}.to_not change(payment, :amount).from(10)
+          context "but none are available" do
+            before { expect(updater).to receive(:ensure_credit_card).and_raise(MissingCardError) }
+
+            it "does not update the payment, sends an payment failure email" do
+              expect(payment).to_not receive(:update_attributes)
+              expect(updater).to receive(:send_payment_failure_email)
+              updater.update!
+            end
+          end
+
+          context "and a credit card is added without errors" do
+            before { expect(updater).to receive(:ensure_credit_card) }
+
+            context "when the payment total doesn't match the outstanding balance on the order" do
+              before { allow(order).to receive(:outstanding_balance) { 5 } }
+              it "updates the payment total to reflect the outstanding balance" do
+                expect{updater.update!}.to change(payment, :amount).from(10).to(5)
+              end
+            end
+
+            context "when the payment total matches the outstanding balance on the order" do
+              before { allow(order).to receive(:outstanding_balance) { 10 } }
+
+              it "does nothing" do
+                expect{updater.update!}.to_not change(payment, :amount).from(10)
+              end
+            end
+          end
+        end
+      end
+    end
+
+    describe "#ensure_credit_card" do
+      let!(:payment) { create(:payment, source: nil) }
+      before { allow(updater).to receive(:payment) { payment } }
+
+      context "when the payment already has a credit card as source" do
+        before { allow(payment).to receive(:source) { double(:credit_card, is_a?: true) }}
+
+        it "returns early" do
+          expect(updater).to_not receive(:credit_card)
+          expect(updater.send(:ensure_credit_card)).to be nil
+        end
+      end
+
+      context "when the payment doesn't have a credit card as source" do
+        before { allow(payment).to receive(:source) { nil } }
+
+        context "when no credit card is specified by the standing order" do
+          before { allow(updater).to receive(:credit_card) { nil } }
+
+          it "raises a MissingCardError" do
+            expect do
+              expect{updater.send(:ensure_credit_card)}.to raise_error MissingCardError
+            end.to_not change(payment, :source).from(nil)
+          end
+        end
+
+        context "when a credit card is specified by the standing order" do
+          let(:credit_card) { create(:credit_card) }
+          before { allow(updater).to receive(:credit_card) { credit_card } }
+
+          it "stores the credit card as the payment source" do
+            expect{updater.send(:ensure_credit_card)}.to change(payment, :source_id).from(nil).to(credit_card.id)
           end
         end
       end
