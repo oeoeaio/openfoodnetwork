556118380a634e42631f0c6b8995206d49cbfbbc
Rob Harrington <oeoeaio@gmail.com>
2017-11-25 19:10:24 +1100

    Add method to initialize a working order for an alteration

--- app/models/alteration.rb
+++ app/models/alteration.rb
@@ -1,4 +1,23 @@
 class Alteration < ActiveRecord::Base
   belongs_to :target_order, class_name: 'Spree::Order'
   belongs_to :working_order, class_name: 'Spree::Order'
+
+  def initialize_working_order
+    self.working_order = Spree::Order.create(working_attrs)
+    target_order.line_items.each do |li|
+      working_order.line_items.create(
+        variant_id: li.variant_id,
+        quantity: li.quantity
+      )
+    end
+  end
+
+  private
+
+  def working_attrs
+    target_order.attributes.slice(
+      "distributor_id",
+      "order_cycle_id"
+    )
+  end
 end
--- spec/models/alteration_spec.rb
+++ spec/models/alteration_spec.rb
@@ -5,4 +5,24 @@ describe Alteration do
     it { expect(subject).to belong_to :target_order }
     it { expect(subject).to belong_to :working_order }
   end
+
+  describe "#initialize_working_order" do
+    let(:enterprise) { create(:enterprise) }
+    let(:order_cycle) { create(:simple_order_cycle, coordinator: enterprise) }
+    let(:target_order) { create(:completed_order_with_totals, distributor: enterprise, order_cycle: order_cycle) }
+    let!(:alteration) { Alteration.new(target_order: target_order) }
+    let(:working_order) { alteration.working_order }
+
+    before do
+      alteration.initialize_working_order
+    end
+
+    it "initializes a new working order and copies appropriate attributes and line items across" do
+      expect(working_order).to be_a Spree::Order
+      expect(working_order.line_items.count).to eq target_order.line_items.count
+      expect(working_order.order_cycle).to eq order_cycle
+      expect(working_order.distributor).to eq enterprise
+      expect(working_order.state).to eq "cart"
+    end
+  end
 end
