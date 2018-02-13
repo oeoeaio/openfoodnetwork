f030c3cd636607154ac2b7a9927885c2bd999c9f
Keir Osborn <keir.osborn@gmail.com>
2018-02-13 10:44:03 +0000

    embedded groups layout changes

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,5 +9,11 @@ class GroupsController < BaseController
     enable_embedded_shopfront
     @hide_menu = true if @shopfront_layout == 'embedded'
     @group = EnterpriseGroup.find_by_permalink(params[:id]) || EnterpriseGroup.find(params[:id])
+    @large_columns = "large-9"
+    @target = "_self"
+
+    return nil unless params[:embedded_shopfront]
+    @large_columns = "large-12"
+    @target = "_blank"
   end
 end
